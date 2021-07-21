7d23cb1923a2de3f2f364b23d8cd3c3cb5d96582
2WDJClass7 <51160431+2WDJClass7@users.noreply.github.com>
2021-07-21 19:47:46 +0900

    7-21

--- 2단계/숫자의표현.py
+++ 2단계/숫자의표현.py
@@ -1,17 +1,18 @@
 def solution(n):
-    count = 0
+    # a, 2a+1, 3a+3 ,4a+6 , 5a+10
+    # 0 , 1, 2 ,3, 4 ...
+    # n으로 나눠서 나머지가 0인 경우만 성립한다
 
-    for i in range(n):  # 0~14
-        a = 1
-        s = i+1  # 현재 숫자
-        while s < n:  # 0 < 15
-            s += a
-            a += 1  # a++
-
-            if s == n:
-                count += 1
-
-    return count
+    a, b = 1, 0  # a: 순서 , b: 추가된 수
+    answer = 0
+    while (n-b)/a > 0:
+        if (n-b) % a == 0:
+            answer += 1
+        b += a
+        a += 1
+    return answer
 
 
-print(solution(10))
+print(solution(15))
+
+# 등차수열 합 공식
