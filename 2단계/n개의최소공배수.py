024fd2dd325c5b6204b0df931794e1de4ceb4f16
2WDJClass7 <51160431+2WDJClass7@users.noreply.github.com>
2021-07-26 20:18:23 +0900

    gcd

--- 2단계/n개의최소공배수.py
+++ 2단계/n개의최소공배수.py
@@ -1,13 +1,35 @@
 def solution(arr):
-    arr2 = []
-    for i in range(len(arr)):
-        for j in range(1, arr[i]+1):
-            # print(j, arr[i])
-            if arr[i] % j == 0:
-                arr2.append(j)
-                print(j)
-    arr2.sort()
-    return arr2
+    for i in range(len(arr)):  # 0 1 2 3
+        if i+1 < len(arr):  # 4
+            a, b = max(arr[i], arr[i+1]), min(arr[i], arr[i+1])
+            t = 1
+            while t > 0:
+                t = a % b
+                a, b = b, t
+            arr[i+1] = arr[i]*arr[i+1]//a
+
+    return arr[-1]
 
 
 print(solution([2, 6, 8, 14]))
+print(solution([1, 2, 3]))
+
+
+# 다른 사람풀이
+# 함수 gcd를 사용
+"""
+
+from fractions import gcd
+
+
+def nlcm(num):      
+    answer = num[0]
+    for n in num:
+        answer = n * answer / gcd(n, answer)
+
+    return answer
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(nlcm([2,6,8,14]));
+
+"""
