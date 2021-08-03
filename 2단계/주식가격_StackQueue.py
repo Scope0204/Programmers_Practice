3834547a7aec64d4886e35c0f20a55910feab193
scope0204 <scope0204@gmail.com>
2021-08-03 20:48:05 +0900

    Use Stack

--- 2단계/주식가격_StackQueue.py
+++ 2단계/주식가격_StackQueue.py
@@ -1,21 +1,37 @@
+# use Stack
 def solution(prices):
-    answer = []
-    for i in range(len(prices)):
-        count = 0
-        if i == len(prices)-1:  # 마지막의 경우 가격변동 x . 무조건 0
-            answer.append(count)
-            break
-        else:
-            for j in range(len(prices)):
-                if j > i:
-                    if prices[i] <= prices[j]:
-                        count += 1
-                    else:
-                        count += 1
-                        break
+    answer = [0]*len(prices)  # 답이 담길 prices 길이의 배열.
+    stack = []
 
-            answer.append(count)
+    for i, price in enumerate(prices):  # i -> index , price -> value
+        # stack이 비었이면 false
+        while stack and price < prices[stack[-1]]:
+            j = stack.pop()
+            answer[j] = i - j
+        stack.append(i)
+
+    # for문 다 돌고 Stac
+    # k에 남아있는 값들 pop
+    while stack:
+        j = stack.pop()
+        answer[j] = len(prices) - 1 - j
+
     return answer
 
 
-print(solution([1, 2, 3, 2, 3]))
+print(solution([1, 2, 2, 2, 3]))
+
+
+# 이중 for문
+"""
+def solution(prices):
+    answer = [0] * len(prices)
+
+    for i in range(len(prices)-1):
+        for j in range(i, len(prices)-1):
+            if prices[i] >prices[j]:
+                break
+            else:
+                answer[i] +=1
+    return answer
+"""
