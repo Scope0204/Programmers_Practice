218080f2fe3f18ffabcfdcb76121a1873bee85bd
scope0204 <scope0204@gmail.com>
2021-08-08 20:57:08 +0900

    08-08

---
+++ 1단계/내적.py
@@ -0,0 +1,21 @@
+def solution(a, b):
+
+    answer = 0
+
+    for i in range(len(a)):
+
+        answer += a[i]*b[i]
+
+    return answer
+
+
+print(solution([1, 2, 3, 4], [-3, -1, 0, 2]))
+
+# 다른 풀이
+# zip() 사용
+"""
+def solution(a, b):
+    
+    return sum([x*y for x, y in zip(a,b)])
+
+"""
