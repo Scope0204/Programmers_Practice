ae807ec9150678295fee59af36bba93c15d09fd1
scope0204 <scope0204@gmail.com>
2021-08-13 00:03:56 +0900

    08-13_Use Dict

--- 1단계/실패율.py
+++ 1단계/실패율.py
@@ -12,7 +12,6 @@
   3-2. 단, N + 1 은 마지막 스테이지(N 번째 스테이지) 까지 클리어 한 사용자를 나타낸다.
 4. 만약 실패율이 같은 스테이지가 있다면 작은 번호의 스테이지가 먼저 오도록 하면 된다.
 5. 스테이지에 도달한 유저가 없는 경우 해당 스테이지의 실패율은 0 으로 정의한다.
-
 """
 
 
@@ -20,15 +19,20 @@ def solution(N, stages):
     users = len(stages)
     a = {}  # 실패율
 
-    for i in range(N):
+    for i in range(1, N+1):
         # 갯수/ 플레이 사용자 수
-        a[i+1] = stages.count(i+1) / users
-        users = users - stages.count(i+1)
+        if stages.count(i):
+            a[i] = stages.count(i) / users
+            users = users - stages.count(i)
+        else:
+            a[i] = 0
 
+    print(a)
     return sorted(a, key=lambda x: a[x], reverse=True)
 
 
-print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
+# print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
+print(solution(4, [4, 4, 4, 4, 4]))
 
 # N : 전체 스테이지 개수, stages : 현재 멈춰있는 스테이지 번호
 # 실패율이 높은 스테이지부터 내림차순으로 스테이지 번호가 담긴 배열 리턴
