2f1ac6c0c2d6c692e9d3c4238edcd2751169d760
scope0204 <scope0204@gmail.com>
2021-08-15 21:02:57 +0900

    08-15

--- 2단계/프린터_StackQueue.py
+++ 2단계/프린터_StackQueue.py
@@ -1,24 +1,41 @@
 def solution(priorities, location):
     wait = {}
     answer = 0
+
     for i in range(len(priorities)):
         wait[i] = priorities[i]
 
-    for i in range(len(priorities)):
-        for j in range(len(priorities)):
-            if i != j and i < j and priorities[i] < priorities[j]:
+    while 1:
+        for i in wait.keys():
+            if wait[i] != max(wait.values()):
                 del wait[i]
                 wait[i] = priorities[i]
                 break
+            elif wait[i] == max(wait.values()):
+                answer += 1
+                if i == location:
+                    return answer
+                else:
+                    del wait[i]
+                break
 
-    # for i in wait.keys():
-    #     answer += 1
-    #     if i == location:
-    #         return answer
 
-    return wait
-
-
-# print(solution([2, 1, 3, 2], 2))
-# print(solution([1, 1, 1, 1, 1, 1], 2))
+print(solution([2, 1, 3, 2], 2))
+print(solution([1, 1, 9, 1, 1, 1], 0))
 print(solution([1, 5, 3, 4, 5, 6], 2))
+
+
+# 다른 풀이: use any()
+""" 
+def solution(priorities, location):
+    queue =  [(i,p) for i,p in enumerate(priorities)]
+    answer = 0
+    while True:
+        cur = queue.pop(0)
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
+        else:
+            answer += 1
+            if cur[0] == location:
+                return answer
+"""
