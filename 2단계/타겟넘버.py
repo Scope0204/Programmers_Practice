17d36bc3e09dc3be068fb8fe4538e60a9dbfd9c0
scope0204 <scope0204@gmail.com>
2021-08-18 19:12:43 +0900

    08-18_DFS/BFS

---
+++ 2단계/타겟넘버.py
@@ -0,0 +1,39 @@
+from collections import deque
+# BFS
+
+
+def solution(numbers, target):
+    answer = 0
+    queue = deque()
+    n = len(numbers)
+    queue.append([numbers[0], 0])
+    queue.append([-1*numbers[0], 0])
+    while queue:
+        temp, idx = queue.popleft()
+        idx += 1
+        if idx < n:
+            queue.append([temp+numbers[idx], idx])
+            queue.append([temp-numbers[idx], idx])
+        else:
+            if temp == target:
+                answer += 1
+    return answer
+
+# dfs
+# def solution(numbers, target):
+#     n = len(numbers)
+#     answer = 0
+#     def dfs(idx, result):
+#         if idx == n:
+#             if result == target:
+#                 nonlocal answer
+#                 answer += 1
+#             return
+#         else:
+#             dfs(idx+1, result+numbers[idx])
+#             dfs(idx+1, result-numbers[idx])
+#     dfs(0,0)
+#     return answer
+
+
+print(solution([1, 1, 1, 1, 1], 3))
