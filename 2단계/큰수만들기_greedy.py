8bb3f054c39627312fbe73072e7fab3e322b7870
scope0204 <scope0204@gmail.com>
2021-08-20 19:40:55 +0900

    08-20

--- 2단계/큰수만들기_greedy.py
+++ 2단계/큰수만들기_greedy.py
@@ -1,25 +1,18 @@
 def solution(number, k):
 
-    a = list(number)
-    b = len(number) - k  # 출력해야할 숫자 길이 = 원래 숫자 길이 - 빼야할 길이
-    answer = ""  # 맞는 숫자를 담는 칸
-    count = 0  # 삭제하지 않은 숫자
+    answer = []
 
-    for i in range(len(number)):
-        now = 0
-        for j in range(count, len(number)):
-            if number[i] < number[j]:
-                if len(number) - k - len(answer) > len(number) - j:
-                    break
+    for i in number:
+        while k > 0 and answer and answer[-1] < i:
+            # k번만큼 해소되지않을 때 , answer이 있을 때 , 현재 answer 값보다 큰 값이 있을 때
+            answer.pop()
+            k -= 1
+        answer.append(i)
 
-    return answer
+    return ''.join(answer[:len(answer)-k])
+    # k가 다 해소되지않는 경우. 중복되는 수의 경우 길이만큼은 맞춰야하기때문
 
 
-print(solution("1231234", 3))
+# print(solution("1231234", 3))
 print(solution("4177252841", 4))
-
-
-# 빼야할 숫자의 조건.
-# 1. 나보다 큰 수가 뒤에 있으면 안됨.
-# 2. 만일 뒤에 있는 경우 해당숫자로부터 출력해야할 숫자길이가 된다면 뺌.
-# 3.
+print(solution("99999", 2))
