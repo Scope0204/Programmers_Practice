8d2a335961f43cfbf240b496a957339a2fef13d4
Scope0204 <scope0204@gmail.com>
2021-11-22 22:27:29 +0900

    11-22

---
+++ DP/9496_스티커.py
@@ -0,0 +1,17 @@
+t = int(input())
+for i in range(t):
+    dp = [] 
+    n = int(input())
+    
+    for k in range(2):
+        dp.append(list(map(int,input().split())))    
+    
+    for j in range(1,n):
+        if j == 1:
+            dp[0][j] += dp[1][j-1]
+            dp[1][j] += dp[0][j-1]
+        else:
+            dp[0][j] += max(dp[1][j-1], dp[1][j-2]) 
+            dp[1][j] += max(dp[0][j-1], dp[0][j-2]) 
+    
+    print(max(dp[0][n-1] , dp[1][n-1]))
