e2c13d6d4a17ea897e41925d0f4b38d0e0a73497
Scope0204 <scope0204@gmail.com>
2021-12-06 15:44:11 +0900

    19_kakao_noLibrary

--- 2단계/후보키.py
+++ 2단계/후보키.py
@@ -35,4 +35,26 @@ print(solution([["100","ryan","music","2"],["200","apeach","math","2"],["300","t
 '''
 https://withcoding.com/77 : 파이썬 set집합 사용법 정리
 https://programmers.co.kr/questions/10412 : 참고 풀이
+
+# 라이브러리 안 쓴 풀이 : https://programmers.co.kr/learn/courses/30/lessons/42890/solution_groups?language=python3
+def solution(relation):
+    answer_list = list()
+    for i in range(1, 1 << len(relation[0])):
+        tmp_set = set()
+        for j in range(len(relation)):
+            tmp = ''
+            for k in range(len(relation[0])):
+                if i & (1 << k):
+                    tmp += str(relation[j][k])
+            tmp_set.add(tmp)
+
+        if len(tmp_set) == len(relation):
+            not_duplicate = True
+            for num in answer_list:
+                if (num & i) == num:
+                    not_duplicate = False
+                    break
+            if not_duplicate:
+                answer_list.append(i)
+    return len(answer_list)
 '''
