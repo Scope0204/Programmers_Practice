f690dd8b7cb82ee197098fcb7e8e2eb806aa1d18
Scope0204 <scope0204@gmail.com>
2021-12-12 20:38:10 +0900

    12-12_DP

--- 3단계/정수삼각형_dp.py
+++ 3단계/정수삼각형_dp.py
@@ -1,20 +1,16 @@
 def solution(triangle):
-    answer = 0
-    map = [v for v in triangle]
-    plus = []
 
-    for i, v in enumerate(triangle):
-        for i2, v2 in enumerate(v):
-            if i == 0:
-                map[1][0] += v[0]
-                map[1][1] += v[0]
-            elif i == len(triangle)-1:
-                break
+    map = [ v for v in triangle]
+
+    for i in range(1, len(triangle)):
+        for j in range(i+1):
+            if j == 0:
+                map[i][j] += map[i-1][j]
+            elif j == i:
+                map[i][j] += map[i-1][j-1]
             else:
-                map[i+1][i2] += v2
-                map[i+1][i2+1] += v2
+                map[i][j] = max(map[i-1][j-1],map[i-1][j]) + triangle[i][j]
 
-    return map
-
+    return max(map[-1])
 
 print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
