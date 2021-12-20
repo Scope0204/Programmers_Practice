b204471b059b3c903e3572d229a4c5b2a848bcca
Scope0204 <scope0204@gmail.com>
2021-12-20 16:02:41 +0900

    12-20

---
+++ 3단계/등굣길.py
@@ -0,0 +1,9 @@
+# puddles는 물에잠긴 지역. 가면안됨
+# 최단경로의 개수를 1,000,000,007로 나눈 나머지를 return
+# BFS/DFS 느낌이 더 강한 문제 
+
+def solution(m, n, puddles):
+    answer = 0
+    return answer
+
+print(solution(4,3,[[2, 2]])) # 4
